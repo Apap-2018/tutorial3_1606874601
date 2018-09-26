81fa1435e48134f64433ae042e71e786a2707da7
mona.natasha <mona_natasha@ymail.com>
2018-09-26 16:26:35 +0700

    done latihan nomor 1

--- src/main/java/com/apap/tutorial3/controller/PilotController.java
+++ src/main/java/com/apap/tutorial3/controller/PilotController.java
@@ -1,9 +1,11 @@
 package com.apap.tutorial3.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.ui.Model;
@@ -25,21 +27,36 @@ public class PilotController {
 		pilotService.addPilot(pilot);
 		return "add";
 	}
-	
+
 	@RequestMapping("/pilot/view")
 	public String view(@RequestParam("licenseNumber") String licenseNumber, Model model) {
 		PilotModel archive = pilotService.getPilotDetailByLicenseNumber(licenseNumber);
-		
+
 		model.addAttribute("pilot", archive);
 		return "view-pilot";
 	}
-	
+
 	@RequestMapping("/pilot/viewall")
 	public String viewall(Model model) {
 		List<PilotModel> archive = pilotService.getPilotList();
-		
+
 		model.addAttribute("listPilot", archive);
 		return "viewall-pilot";
+	}
+
+	@RequestMapping(value = { "/pilot/view/license-number", "pilot/view/license-number/{licenseNumber}" })
+	public String viewPath(@PathVariable Optional<String> licenseNumber, Model model) {
+		if (licenseNumber.isPresent()) {
+			List<PilotModel> archive = pilotService.getPilotList();
+
+			for (PilotModel pilot : archive) {
+				if (pilot.getLicenseNumber().equals(licenseNumber.get())) {
+					model.addAttribute("pilot", pilot);
+					return "view-pilot-license";
+				}
+			}
+		}
+		return "view-pilot-license-error";
 	}
 
 }
